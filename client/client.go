7ff13ebb2479f5e481ac43118de695a414a961b8
josopondo <josephineopondo5@gmail.com>
2024-09-23 15:39:51 +0300

    listens for user input and server messages concurrently

--- client/client.go
+++ client/client.go
@@ -8,6 +8,11 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <address>")
+		return
+	}
+
 	conn, err := net.Dial("tcp", os.Args[1])
 	if err != nil {
 		fmt.Printf("Oops! %v\n", err)
@@ -17,21 +22,36 @@ func main() {
 
 	userInput := make(chan string)
 
+	// Goroutine for reading user input
 	go func() {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
 			input := scanner.Text()
+			if input == "exit" {
+				close(userInput)
+				return
+			}
 			userInput <- input
 		}
 	}()
 
+	// Goroutine for reading server messages
 	go func() {
 		reader := bufio.NewReader(conn)
-		msg, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Disconnected:", err)
-			return
+		for {
+			msg, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Disconnected:", err)
+				break
+			}
+			fmt.Println("👉 ", msg)
 		}
-		fmt.Println(msg)
 	}()
+
+	// Main loop for sending user input to the server
+	for input := range userInput {
+		fmt.Fprintf(conn, input+"\n")
+	}
+
+	fmt.Println("Goodbye buddies!")
 }
